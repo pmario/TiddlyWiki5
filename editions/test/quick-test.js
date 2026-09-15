66391f9ad029f54c404cffc5c9bf23c76286e4ac
pmario <pmariojo@gmail.com>
2026-09-15 19:47:05 +0200

    Fix the usage comment in quick-test.js

--- editions/test/quick-test.js
+++ editions/test/quick-test.js
@@ -2,13 +2,13 @@
 Fast test runner that boots the test edition and runs selected test specs.
 Skips the expensive --rendertiddler step from --build index.
 
-Usage:
-  node quick-test.js [test-files...]
+Usage, from the repository root:
+	node editions/test/quick-test.js [test-files...]
 
 Examples:
-  node quick-test.js                                     # Run ALL specs
-  node quick-test.js test-wikitext-parser                # Run one spec file
-  node quick-test.js test-wikitext-parser test-filters   # Run multiple spec files
+	node editions/test/quick-test.js # Run ALL specs
+	node editions/test/quick-test.js test-wikitext-parser # Run one spec file
+	node editions/test/quick-test.js test-wikitext-parser test-filters # Run multiple spec files
 */
 
 "use strict";
